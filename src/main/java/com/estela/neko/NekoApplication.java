e431c36bcf92c74a654b8a9955a3c6b66891393b
fuming.lj <fuming.lj@alibaba-inc.com>
2018-07-23 19:45:51 +0800

    3ewdsds

---
+++ src/main/java/com/estela/neko/NekoApplication.java
@@ -0,0 +1,12 @@
+package com.estela.neko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NekoApplication.class, args);
+    }
+}
